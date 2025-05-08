9c503cd6891c6f3995dd70e57a79c0e6a7d48292
Obinnaojukwu <gd064604@gmail.com>
2025-05-08 19:05:35 +0100

    week01

--- server.js
+++ server.js
@@ -2,29 +2,43 @@
  * This server.js file is the primary file of the 
  * application. It is used to control the project.
  *******************************************/
+
 /* ***********************
  * Require Statements
  *************************/
-const express = require("express")
-const env = require("dotenv").config()
-const app = express()
-const static = require("./routes/static")
+const express = require("express");
+const env = require("dotenv").config();
+const path = require("path");
+const app = express();
+
+/* ***********************
+ * View Engine and Views
+ *************************/
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+/* ***********************
+ * Static Files Middleware
+ *************************/
+app.use(express.static(path.join(__dirname, "public")));
 
 /* ***********************
  * Routes
  *************************/
-app.use(static)
+// Home page route
+app.get("/", (req, res) => {
+  res.render("index");
+});
 
 /* ***********************
  * Local Server Information
- * Values from .env (environment) file
  *************************/
-const port = process.env.PORT
-const host = process.env.HOST
+const port = process.env.PORT || 3000;
+const host = process.env.HOST || "localhost";
 
 /* ***********************
- * Log statement to confirm server operation
+ * Start Server
  *************************/
 app.listen(port, () => {
-  console.log(`app listening on ${host}:${port}`)
-})
+  console.log(`app listening on ${host}:${port}`);
+});
