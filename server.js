bca07a82db0aaf24b98beb3a841badfe503dbf8d
OBINNA OJUKWU <gd064604@gmail.com>
2025-07-18 23:23:28 +0100

    week ones rework

--- server.js
+++ server.js
@@ -2,43 +2,42 @@
  * This server.js file is the primary file of the 
  * application. It is used to control the project.
  *******************************************/
-
 /* ***********************
  * Require Statements
  *************************/
-const express = require("express");
-const env = require("dotenv").config();
-const path = require("path");
-const app = express();
+const express = require("express")
+const expressLayouts = require("express-ejs-layouts")
+const env = require("dotenv").config()
+const app = express()
+const static = require("./routes/static")
 
 /* ***********************
- * View Engine and Views
+ * View Engines and Templates 
  *************************/
-app.set("view engine", "ejs");
-app.set("views", path.join(__dirname, "views"));
-
-/* ***********************
- * Static Files Middleware
- *************************/
-app.use(express.static(path.join(__dirname, "public")));
+app.set("view engine", "ejs")
+app.use(expressLayouts)
+app.set("layout", "./layouts/layout") // not at views root
 
 /* ***********************
  * Routes
  *************************/
-// Home page route
-app.get("/", (req, res) => {
-  res.render("index");
-});
+app.use(static)
+
+// Index route
+app.get("/", function(req, res) {
+  res.render("index", {title: "Home"})
+})
 
 /* ***********************
  * Local Server Information
+ * Values from .env (environment) file
  *************************/
-const port = process.env.PORT || 3000;
-const host = process.env.HOST || "localhost";
+const port = process.env.PORT
+const host = process.env.HOST
 
 /* ***********************
- * Start Server
+ * Log statement to confirm server operation
  *************************/
 app.listen(port, () => {
-  console.log(`app listening on ${host}:${port}`);
-});
+  console.log(`app listening on ${host}:${port}`)
+})
