5b9d3a2bb51811af1e277052f69d7042d4761517
OBINNA OJUKWU <gd064604@gmail.com>
2025-07-26 09:46:29 +0100

    week03

--- server.js
+++ server.js
@@ -12,6 +12,10 @@ const app = express()
 const static = require("./routes/static")
 const baseController = require("./controllers/baseController")
 const inventoryRoute = require("./routes/inventoryRoute");
+const utilities = require('./utilities/index')
+const session = require("express-session")
+const pool = require('./database/')
+
 
 /* ***********************
  * View Engines and Templates 
@@ -29,6 +33,25 @@ app.use(static)
 app.get("/", baseController.buildHome) 
 // Inventory routes
 app.use("/inv", inventoryRoute)
+
+/* ***********************
+* Express Error Handler
+* Place after all other middleware
+*************************/
+app.use(async (err, req, res, next) => {
+  let nav = await utilities.getNav()
+  console.error(`Error at: "${req.originalUrl}": ${err.message}`)
+  if (err.status == 404) {
+    message = err.message
+  } else {
+    message = "Oh no! There was a crash. Maybe try a different route?"
+  }
+  res.render("errors/error", {
+    title: err.status || "Server Error",
+    message,
+    nav,
+  })
+})
 
 /* ***********************
  * Local Server Information
