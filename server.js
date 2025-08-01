6d220b2a54357fad9497a9254ef06731ea903403
OBINNA OJUKWU <gd064604@gmail.com>
2025-08-01 01:22:04 +0100

    week04

--- server.js
+++ server.js
@@ -11,28 +11,86 @@ const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
 const baseController = require("./controllers/baseController")
-const inventoryRoute = require("./routes/inventoryRoute");
-const utilities = require('./utilities/index')
+const inventoryRoute = require("./routes/inventoryRoute")
+const accountRoute = require("./routes/accountRoute")
+const utilities = require("./utilities/")
 const session = require("express-session")
 const pool = require('./database/')
-
+const bodyParser = require("body-parser")
+const cookieParser = require("cookie-parser")
 
 /* ***********************
- * View Engines and Templates 
+ * Middleware
+ * Between the request and response
+ * ************************/
+// Unit 4, Sessions & Messages Activity
+app.use(
+  session({
+    store: new (require("connect-pg-simple")(session))({
+      createTableIfMissing: true,
+      pool,
+    }),
+    secret: process.env.SESSION_SECRET,
+    resave: true,
+    saveUninitialized: true,
+    name: "sessionId",
+  })
+)
+// Unit 4, Sessions & Messages Activity
+// Express Messages Middleware
+app.use(require("connect-flash")())
+app.use(function (req, res, next) {
+  res.locals.messages = require("express-messages")(req, res)
+  next()
+})
+// Unit 4, Process Registration Activity
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+
+
+// Express Messages Middleware
+app.use(require('connect-flash')())
+app.use(function(req, res, next){
+  res.locals.messages = require('express-messages')(req, res)
+  next()
+})
+
+// Unit 4, Process Registration Activity
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+
+// Unit 5 Authentication cookie use
+app.use(cookieParser())
+
+/* ***********************
+ * View Engine And Templates
  *************************/
 app.set("view engine", "ejs")
 app.use(expressLayouts)
 app.set("layout", "./layouts/layout") // not at views root
 
+
+
+
+
 /* ***********************
  * Routes
  *************************/
 app.use(static)
-
-// Index route
-app.get("/", baseController.buildHome) 
-// Inventory routes
+// Index route - Unit 3, Robust Error Handling activity
+app.get("/", utilities.handleErrors(baseController.buildHome))
+// Inventory routes - Unit 3, Build Inventory route activity
 app.use("/inv", inventoryRoute)
+// Account routes - Unit 4, Deliver Login activity
+app.use("/account", accountRoute)
+
+
+
+// File Not Found Route - must be last route in list
+app.use(async (req, res, next) => {
+  next({status: 404, message: 'Sorry, we appear to have lost that page.'})
+})
+
 
 /* ***********************
 * Express Error Handler
@@ -52,6 +110,9 @@ app.use(async (err, req, res, next) => {
     nav,
   })
 })
+
+
+
 
 /* ***********************
  * Local Server Information
